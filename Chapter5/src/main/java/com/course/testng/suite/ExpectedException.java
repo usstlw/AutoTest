2eca0208e75d908ac6f801718f5f8c774725a239
usstlw <usstlw@126.com>
2020-01-19 20:33:28 +0800

    update

--- Chapter5/src/main/java/com/course/testng/suite/ExpectedException.java
+++ Chapter5/src/main/java/com/course/testng/suite/ExpectedException.java
@@ -1,4 +1,19 @@
 package com.course.testng.suite;
 
+import org.testng.annotations.Test;
+
 public class ExpectedException {
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void runTimeExceptionFailed(){
+        System.out.println("这是一个失败的异常测试");
+    }
+
+    //这是一个成功的异常测试
+    @Test(expectedExceptions = RuntimeException.class)
+    public void runTimeExceptionSuccess(){
+        System.out.println("这是我的异常测试");
+        throw new RuntimeException();
+
+    }
 }
